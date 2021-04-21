0445f9ad01b59b5aa9cf6f7305abe8fcc59d7431
Ashishamar99 <ashishamar1999@gmail.com>
2021-04-21 15:29:16 +0530

    Added Form Validator.

--- QuizQuesAngular/quizques/src/app/test/test.component.ts
+++ QuizQuesAngular/quizques/src/app/test/test.component.ts
@@ -1,67 +1,63 @@
 import { Component, OnInit, Output } from '@angular/core';
 import { EventEmitter } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Test } from 'src/models/test';
 import { TestService } from '../services/testservice';
 
 @Component({
-  selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.scss']
+    selector: 'app-test',
+    templateUrl: './test.component.html',
+    styleUrls: ['./test.component.scss']
 })
+
 export class TestComponent implements OnInit {
 
-  testArr: Test[] = [];
-  testService: TestService; //testService is the variable we use.
-  @Output() addNotificationCount: EventEmitter<number>; //Step 1
+    testArr: Test[] = [];
+    testService: TestService;
+    @Output() addNotificationCount: EventEmitter<number>;
 
-  testForm = new FormGroup({
-    id: new FormControl('Select a value'),
-    testName: new FormControl(''),
-    marks: new FormControl(0),
-    noOfQues: new FormControl(0)
-  });
+    testForm = this.formBuilder.group({
+        id: 0,
+        testName: ['', Validators.required],
+        marks: 0,
+        noOfQues: 0
+        });
 
-  subjectForm = new FormGroup({
-    subjectName:  new FormControl(''),
-    subjectMarks:  new FormControl(0),
-    noOfQuesForSubject:  new FormControl(0)
-  });
-  
-  constructor(testServiceParam: TestService) {
-    this.testService = testServiceParam; 
+    constructor(testService: TestService, private formBuilder: FormBuilder) {
+        this.testService = testService; 
+        
+        this.addNotificationCount = new EventEmitter<number>();
+        
+    }
+        
+    ngOnInit(): void {
+        this.getAllTests();
+    }
 
-    this.addNotificationCount = new EventEmitter<number>(); //Step 2
-  }
+    addNotification(){
+        this.addNotificationCount.emit(1);
+    }
 
-  ngOnInit(): void {
-    this.getAllTests();
-  }
+    getAllTests(){
+        this.testArr = this.testService.getTests();
+        console.log(this.testArr);
+    }
 
-  addNotification(){                                   //Step 3 // Method to call and generate these notifications. //Step 4 in test.component.html
-    this.addNotificationCount.emit(1);
-  }
+    onSubmit(){
+        let valSubmitted = this.testForm.value;
+        console.warn('Test details have been submitted', valSubmitted);
+        console.log('Test Name Required Check ' + this.testForm.hasError('required', 'testName'));
+    
+        let arrLastId = this.testArr.reverse()[0].id + 1;
+        this.testArr.reverse();
+        let t = new Test(arrLastId, valSubmitted.testName, valSubmitted.marks, valSubmitted.noOfQues);
+        this.testArr.push(t);
+        console.log('New testArr = ', this.testArr)
+    
+        this.testForm.reset();
+    }
 
-  getAllTests(){
-    this.testArr = this.testService.getTests();
-    console.log(this.testArr);
-  }
-  //Custom Methods !!!
-
-  onSubmit(){
-    console.warn('Test details have been submitted', this.testForm.value);
-    this.testForm.reset();
-  }
-
-  onChangeType(event, id){
-    //id=$event.target.value
-    //console.log(event);
-    console.log(id);
-  }
-
-  onSubjectSubmit(){
-    console.warn('Subject details have been submitted', this.subjectForm.value);
-    this.subjectForm.reset();
-  }
-
+    onChangeType(event, id){
+        console.log(id);
+    }
 }
